50c7a025a47c28d8aade5202fa271972d56aabbc
Alexander <a.ayugov@zebrains.team>
2023-09-12 17:40:32 +0400

    second version

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,126 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useFormik } from "formik";
 
+
+
+const validateEmail = (value) =>{
+  if (!value) {
+    return'*Обязательное поле';
+  } 
+  else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+    return 'Email не валидный';
+  }
+  return null;
+}
+
+const validateName = (value) => !value ? '*Обязательное поле': null
+
+const validatePassword = (value) =>{
+
+  if (!value) {
+    return "*Обязательное поле";
+  } 
+  else if (value.length < 2) {
+    return "Пароль должен содержать как минимум 2 символов";
+  }
+  else if (value === value.toLowerCase()){
+    return "Пароль должен содержать заглавные буквы";
+  }
+  return null;
+}
+
+const validateConfirmPassword = (value, password) => {
+  if (!value) {
+    return "*Обязательное поле";
+  } else if (value !== password) {
+    return "Пароли не совпадают";
+  }
+  return null;
+};
+
+
+const validate = (values) =>{
+  const errors = {}
+  errors.name = validateName(values.name)
+  errors.email = validateEmail(values.email)
+  errors.password = validatePassword(values.password)
+  errors.confirmPassword = validateConfirmPassword(values.confirmPassword, values.password)
+
+  return errors;
+}
 
 function App() {
-
-  const validateEmail = (value) =>{
-    if (!value) {
-      return'*Обязательное поле';
-    } 
-    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-      return 'Email не валидный';
+  const formik = useFormik({
+    initialValues: {
+      name:'',
+      email: '', 
+      password: '' ,
+      confirmPassword: '',
+    },
+    validate,
+    onSubmit: values => {
+      console.log(values)
     }
-    return null;
-  }
-
-  const validateName = (value) => !value ? '*Обязательное поле': null
-
-  const validatePassword = (value) =>{
-
-    if (!value) {
-      return "*Обязательное поле";
-    } 
-    else if (value.length < 2) {
-      return "Пароль должен содержать как минимум 2 символов";
-    }
-    else if (value === value.toLowerCase()){
-      return "Пароль должен содержать заглавные буквы";
-    }
-    return null;
-  }
-
-  const validateConfirmPassword = (value, password) => {
-    if (!value) {
-      return "*Обязательное поле";
-    } else if (value !== password) {
-      return "Пароли не совпадают";
-    }
-    return null;
-  };
+    
+  })
 
 
   return (
     <div className="wrapper">
       <h1>My Form</h1>
-      <Formik
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <div className="item">
+          <input 
+            className='input'
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          {formik.errors.name && formik.touched.name && <p>{formik.errors.name}</p>}
+        </div>
 
-        initialValues={{ 
-          name:'',
-          email: '', 
-          password: '' ,
-          confirmPassword: '',
-        }}
+        <div className="item">
+          <input 
+            className='input'
+            type="email"
+            onBlur={formik.handleBlur}
+            name="email"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.errors.email && formik.touched.email && <p>{formik.errors.email}</p>}
+        </div>
 
-        validate={values => {
-          const errors = {}
+        <div className="item">
+          <input 
+            className='input'
+            type="password"
+            name="password"
+            placeholder="Password"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {formik.errors.password && formik.touched.password && <p>{formik.errors.password}</p>}
+        </div>
 
-          errors.name = validateName(values.name)
-          errors.email = validateEmail(values.email)
-          errors.password = validatePassword(values.password)
-          errors.confirmPassword = validateConfirmPassword(values.confirmPassword, values.password)
+        <div className="item">
+          <input 
+            className='input'
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm your password"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.confirmPassword}
+          />
+          {formik.errors.confirmPassword && formik.touched.confirmPassword && <p>{formik.errors.confirmPassword}</p>}
+        </div>
 
-          return errors;
-        }}
-
-        onSubmit={(values) => console.log("Form is validated! Submitting the form...", values)}
-
-      >
-
-        {() => (
-          <Form className="form">
-            <Field className='input' type="text" name="name" placeholder="Enter your name"/>
-            <ErrorMessage name="name" component="div" />
-
-            <Field className='input' type="email" name="email" placeholder="Email"/>
-            <ErrorMessage name="email" component="div" />
-
-            <Field className='input' type="password" name="password" placeholder="Password"/>
-            <ErrorMessage name="password" component="div" />
-
-            <Field className='input' type="password" name="confirmPassword" placeholder="Confirm your password"/>
-            <ErrorMessage name="confirmPassword" component="div" />
-
-            <button className='button' type="submit">
-              Submit
-            </button>
-          </Form>
-        )}
-
-      </Formik>
+        <button className="button" type="submit">Submit</button>
+      </form>
     </div>
   )
 }
